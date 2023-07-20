28ca1ee1e9f7bc2ce0147ef39ebcf09622eeb69b
Sabari <r.sabarinathan02@gmail.com>
2023-07-20 21:26:25 +0530

    url updation

--- client/src/assets/url.js
+++ client/src/assets/url.js
@@ -1,4 +1,4 @@
-// const base_url = "https://flightbookingbackend.onrender.com"
-const base_url = "http://localhost:5000"
+const base_url = "https://flightbooking-api.vercel.app/"
+// const base_url = "http://localhost:5000"
 
 export default base_url;
