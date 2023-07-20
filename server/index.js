3bbc13b0cc70fea2b4c9a9078ff90d45bfedece8
Sabari <r.sabarinathan02@gmail.com>
2023-07-20 21:39:12 +0530

    cors

--- server/index.js
+++ server/index.js
@@ -11,7 +11,11 @@ app.get("/",(req,res) =>{
 const dbConfig = require("./config/dbConfig");
 const bodyParser = require("body-parser");
 
-app.use(cors());
+app.use(cors({
+    origin: '*',
+    methods: ['GET', 'POST', 'PUT','DELETE'],
+    headers: ['Authorization'],
+}));
 app.use(express.json());
 app.use(bodyParser.json());
 
